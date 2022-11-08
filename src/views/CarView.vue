aa3a1e79a1f0b294a820cbb08134817174bf6cbc
ergunDevv <ergncyln2127088@gmail.com>
2022-11-08 11:11:11 +0300

    Routes added about,cars

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,15 +1,22 @@
+<script setup>
+import { useRoute } from "vue-router";
+import cars from '../data/cars.json'
+
+
+
+const route = useRoute();
+const car = cars.find(c=>c.id ===parseInt(route.params.id))
+
+console.log(route.params);
+</script>
+
+
 <template>
-    <div>
-            <h1>Car View</h1>
-    </div>
+  <div>
+    <h1>Name : {{car.name}}</h1>
+    <h1>Year : {{car.year}}</h1>
+    <h1>Price : {{car.price}}</h1>
+  </div>
 </template>
 
-<script setup>
-import { useRoute } from 'vue-router';
-
-    const route = useRoute();
-
-    console.log(route.params)
-
-</script>
 
